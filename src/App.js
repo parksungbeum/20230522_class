256c18a5363e3cdf923ae819ce3985fddb112bb1
psb3313 <psb3313@naver.com>
2023-05-22 10:08:35 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Text, Button} from "@chakra-ui/react";
 
 function App() {
   return (
-   <div>APP</div>
+    <>
+   <Text fontWeight={600} color={"red.500"}>APP</Text>
+   <Button colorScheme='blue'>Button</Button>
+   <Button colorScheme='teal' variant='outline' > Button</Button>
+  </>
   );
 }
 
